ae7f8eb4dc8c10adcfcbfab0eaf75c811ed0c26b
sandykrishdaswani <51950647+sandykrishdaswani@users.noreply.github.com>
2019-07-10 22:38:11 +0530

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,7 +1,5 @@
-tan1=int(input())
-san1=input().split()
-for i in range(len(san1)):
-    for j in range(i+1,len(san1)):
-        for k in range(j+1,len(san1)):
-            if(int(san1[i])+int(san1[j])==int(san1[k])):
-                print(san1[i],san1[j],san1[k])
+san1  =input()
+tan1 = list(map(int,input().split()))
+for i in range(len(tan1)):
+    if (i%2 == 0 and tan1[i]%2 !=0) or (i%2!= 0 and tan1[i]%2 == 0):
+        print(tan1[i],end = " ")
