949fc8b83a09107d213beb15ca7fa5bb5e4338c9
sandykrishdaswani <51950647+sandykrishdaswani@users.noreply.github.com>
2019-07-10 19:48:19 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,12 @@
+sandy=list(map(str,input()))
+v=t=0
+for i in range(0,len(sandy)-1):
+  q1=n[i]
+  if int(q1)!=0:
+   for j in range(i+1,i+2):
+    q1=q1+n[j]
+    if int(q1)<27 and int(q1)>0: v=v+1
+    elif int(q1)==0: v=v-1
+    else: break
+if v!=1: t=v%2
+print(v+t+1)
