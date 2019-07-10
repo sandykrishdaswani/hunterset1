ef5ed1f3bc39bfe40f2aafc9958126007dccae47
sandykrishdaswani <51950647+sandykrishdaswani@users.noreply.github.com>
2019-07-10 19:49:56 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -4,7 +4,7 @@ for i in range(0,len(sandy)-1):
   q1=sandy[i]
   if int(q1)!=0:
    for j in range(i+1,i+2):
-    q1=q1+n[j]
+    q1=q1+sandy[j]
     if int(q1)<27 and int(q1)>0: v=v+1
     elif int(q1)==0: v=v-1
     else: break
