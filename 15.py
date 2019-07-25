164867d4795e941c19d1e8423aa1dfa9fbaeaa2a
sandykrishdaswani <51950647+sandykrishdaswani@users.noreply.github.com>
2019-07-25 16:49:16 +0530

    Create 15.py

---
+++ 15.py
@@ -0,0 +1,11 @@
+sandy=int(input())
+l=list(map(int, input().split()))[:n]
+mx=max(l)
+funl=[]
+for i in range(0,len(l)):
+    new=l[i:]
+    fix=max(new)
+    if(l[i]==fix):
+        funl.append(l[i])
+print(*funl)
+print(mx)
