7a79dd423d31b30543efd8b26462d53e6035e8a0
sandykrishdaswani <51950647+sandykrishdaswani@users.noreply.github.com>
2019-07-26 14:46:55 +0530

    Update 18.py

--- 18.py
+++ 18.py
@@ -1,10 +1,21 @@
-sandy,krish=map(int,input().split())
-for i in range(sandy,krish):
-  sum=0
-  temp=i
-  while(temp>0):
-    c=temp%10
-    sum=sum+c**3
-    temp=temp//10
-  if(i==sum):
-    print(sum,end=" ")
+sandy=int(input())
+n1,n2=[],[]
+c=0
+for i in range(0,sandy):
+  n1.append(list(map(int,input().split())))
+for i in range(0,sandy+2):
+  if i==0:
+    n2.append([0]*(sandy+2))
+  elif i==(sandy+2)-1:
+    n2.append([0]*(sandy+2))
+  else:
+    n2.append([0]+n1[i-1]+[0])
+for i in range(0,len(n2)):
+  for j in range(0,len(n2)):
+    if n2[i][j]!=0 and sandy%2==0:
+      if n2[i-1][j-1]==0 and n2[i-1][j]==0 and n2[i-1][j+1]==0 and n2[i][j-1]==0 and n2[i][j+1]==0 and n2[i+1][j-1]==0 and n2[i+1][j]==0 and n2[i+1][j+1]==0:
+        c+=1
+    elif n2[i][j]!=0 and sandy%2!=0:
+      if n2[i-1][j]==0 and n2[i+1][j]==0 and n2[i][j-1]==0 and n2[i][j+1]==0:
+        c+=1
+print(c)
